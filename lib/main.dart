daf25ad4e0b4b21b28fc06e4bc4f77832a72005d
paswi <paw.swiniarski@gmail.com>
2020-04-05 20:37:31 +0200

    testing Git

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Czemu trzeba pracowac'),
     );
   }
 }
@@ -92,7 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Jak zyc?',
             ),
             Text(
               '$_counter',
